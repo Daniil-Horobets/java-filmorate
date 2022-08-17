18a5a75430f5d24667e02d5e548e5b2aa09df267
golovkinen <golovkinen@yandex.ru>
2022-08-18 01:41:58 +0300

    add-director

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -4,12 +4,13 @@ import lombok.*;
 import net.minidev.json.annotate.JsonIgnore;
 
 import java.time.LocalDate;
-import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@RequiredArgsConstructor
 @NoArgsConstructor
 @EqualsAndHashCode(of = "id")
 public class Film {
@@ -20,7 +21,8 @@ public class Film {
     private int duration;
     private Mpa mpa;
     private Set<Genre> genres;
+    private List<Director> directors;
 
     @JsonIgnore
-    private Set<Integer> likedUsersIds = new HashSet<>();
+    private Set<Integer> likedUsersIds;
 }
