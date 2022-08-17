7963b9ca5de039fc5d1b8e731e6f5d03d2d42d88
Alex <str>
2022-08-17 17:19:03 +0300

    feat: add events feed

---
+++ src/main/java/ru/yandex/practicum/filmorate/storage/EventStorage.java
@@ -0,0 +1,12 @@
+package ru.yandex.practicum.filmorate.storage;
+
+import ru.yandex.practicum.filmorate.model.Event;
+
+import java.util.List;
+
+public interface EventStorage {
+
+    Event createEvent(Event event);
+
+    List<Event> getUserEvents(int user_id);
+}
