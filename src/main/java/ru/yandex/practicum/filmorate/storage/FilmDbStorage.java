ce93baeefc827e87f91870b38b226d2b976c3c98
Dmitry <dimm1975@yandex.ru>
2022-08-20 12:21:19 +0300

    fix: codestyle SQL ref: сократил условие в getFilmsByQuery

--- src/main/java/ru/yandex/practicum/filmorate/storage/FilmDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/FilmDbStorage.java
@@ -52,9 +52,9 @@ public class FilmDbStorage implements FilmStorage {
     public List<Film> getFilmsByQuery (String query, List<String> by) {
         if (by.size() == 1 && by.get(0).equals("title")) {
             final String sqlQuery =
-                    "select * " +
-                            "from films f " +
-                            "join mpa m on f.film_mpa_id = m.mpa_id " +
+                    "SELECT * " +
+                            "FROM films f " +
+                            "JOIN mpa m ON f.film_mpa_id = m.mpa_id " +
                             "where locate(lower(?), lower(f.film_name))";
             List<Film> films = jdbcTemplate.query(sqlQuery, this::mapToFilm, query);
             for (Film film : films) {
@@ -66,12 +66,12 @@ public class FilmDbStorage implements FilmStorage {
         }
         if (by.size() == 1 && by.get(0).equals("director")) {
             final String sqlQuery =
-                    "select * " +
-                            "from films f " +
-                            "join mpa m on f.film_mpa_id = m.mpa_id " +
-                            "left join FILM_DIRECTORS FD on f.FILM_ID = FD.FILM_ID " +
-                            "left join DIRECTORS D on D.DIRECTOR_ID = FD.DIRECTOR_ID " +
-                            "where locate(lower(?), lower(d.DIRECTOR_NAME))";
+                    "SELECT * " +
+                            "FROM films f " +
+                            "JOIN mpa m ON f.film_mpa_id = m.mpa_id " +
+                            "LEFT JOIN film_directors fd on f.film_id = fd.film_id " +
+                            "LEFT JOIN directors d ON d.director_id = fd.director_id " +
+                            "WHERE locate(lower(?), lower(d.director_name))";
             List<Film> films = jdbcTemplate.query(sqlQuery, this::mapToFilm, query);
             for (Film film : films) {
                 genreDbStorage.loadFilmGenre(film);
@@ -80,15 +80,14 @@ public class FilmDbStorage implements FilmStorage {
             }
             return films;
         }
-        if ((by.size() == 2 && by.get(0).equals("title") && by.get(1).equals("director")) ||
-                ((by.size() == 2 && by.get(0).equals("director") && by.get(1).equals("title")))) {
+        if (by.containsAll((List.of("title", "director")))) {
             final String sqlQuery =
-                    "select * " +
-                            "from films f " +
-                            "join mpa m on f.film_mpa_id = m.mpa_id " +
-                            "left join FILM_DIRECTORS FD on f.FILM_ID = FD.FILM_ID " +
-                            "left join DIRECTORS D on D.DIRECTOR_ID = FD.DIRECTOR_ID " +
-                            "where locate(lower(?), lower(d.DIRECTOR_NAME)) or locate(lower(?), lower(f.FILM_NAME))";
+                    "SELECT * " +
+                            "FROM films f " +
+                            "JOIN mpa m ON f.film_mpa_id = m.mpa_id " +
+                            "LEFT JOIN film_directors fd on f.film_id = fd.film_id " +
+                            "LEFT JOIN directors d ON d.director_id = fd.director_id " +
+                            "WHERE locate(lower(?), lower(d.director_name)) or locate(lower(?), lower(f.film_name))";
             List<Film> films = jdbcTemplate.query(sqlQuery, this::mapToFilm, query, query);
             for (Film film : films) {
                 genreDbStorage.loadFilmGenre(film);
