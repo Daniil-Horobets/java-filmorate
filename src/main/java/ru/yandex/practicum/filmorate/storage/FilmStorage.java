20f069b1796318e850bf8a76afc8b84366263869
DaniilGorobets <topiccuppts@gmail.com>
2022-08-24 15:29:30 +0200

    fix: add director fix

--- src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
@@ -4,17 +4,20 @@ import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.model.User;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface FilmStorage {
     List<Film> getAll();
     Film get(int id);
-    public List<Film> getFilmsByQuery (String query, List<String> by);
+    List<Film> getFilmsByQuery (String query, List<String> by);
     Film create(Film film);
     Film update(Film film);
     void addLike(User user, Film film);
     void deleteLike(User user, Film film);
     List<Film> readBestDirectorFilms(int directorId, String param);
     List<Film> getCommonFilms(int userId, int friendId);
-    public List<Film> getRecommendations (Integer id);
+    List<Film> getRecommendations (Integer id);
     boolean delete(int id);
+
+    List<Film> getMostLikedFilms(Integer count, Optional<Integer> genreId, Optional<Integer> year);
 }
